81ef6aef840fed71abe00e119bcd6539b3325e52
Sunny Lee <leesunny507@gmail.com>
2020-05-21 21:55:15 -0400

    changed database to heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     API_KEY: process.env.REACT_APP_API_KEY, 
-    APIEndpoint: 'http://localhost:8000/api', 
-    //APIEndpoint: 'https://fierce-plains-54443.herokuapp.com/api',
+    //APIEndpoint: 'http://localhost:8000/api', 
+    APIEndpoint: 'https://fierce-plains-54443.herokuapp.com/api',
 }
